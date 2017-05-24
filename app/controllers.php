4ac86e3a16bef21cb5925efdb342fea0b97fb0fa
Yoan Haouzi <yoan.haouzi@evaneos.com>
2017-05-24 11:53:12 +0200

    refacto : Implement n-layer archi

--- app/controllers.php
+++ app/controllers.php
@@ -2,10 +2,18 @@
 
 use ControllerProvider\PokemonControllerProvider;
 use ServiceProvider\ControllerServiceProvider;
+use ServiceProvider\PokemonServiceProvider;
 use Silex\Application;
+use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 /** @var Application $app */
+$app->register(new PokemonServiceProvider());
 $app->register(new ControllerServiceProvider());
 
 // Pokemon
 $app->mount('/', new PokemonControllerProvider());
+
+$app->error(function (\Exception $e, Request $request, $code) use ($app) {
+    return new Response($e->getMessage() . PHP_EOL . $e->getTraceAsString(), $code);
+});
--- src/Controllers/PokemonController.php
+++ src/Controllers/PokemonController.php
@@ -2,26 +2,28 @@
 
 namespace Evaneos\Archi\Controllers;
 
-use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DBALException;
-use Psr\Log\InvalidArgumentException;
+use Evaneos\Archi\Services\PokemonService;
+use Evaneos\Archi\ValueObjects\Pokemon;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class PokemonController
 {
-    /** @var Connection */
-    private $connection;
+    /**
+     * @var PokemonService
+     */
+    private $pokemonService;
 
     /**
      * PokemonController constructor.
      *
-     * @param Connection $connection
+     * @param PokemonService $pokemonService
      */
-    public function __construct(Connection $connection)
+    public function __construct(PokemonService $pokemonService)
     {
-        $this->connection = $connection;
+        $this->pokemonService = $pokemonService;
     }
 
     /**
@@ -33,10 +35,8 @@ class PokemonController
      */
     public function pokedex(Request $request)
     {
-        $sql = 'SELECT uuid, type, level FROM pokemon.collection';
-        $query = $this->connection->query($sql);
-
-        return new JsonResponse([$query->fetchAll()]);
+        $pokemons = $this->pokemonService->getAll();
+        return new JsonResponse([$pokemons]);
     }
 
     /**
@@ -49,7 +49,7 @@ class PokemonController
      */
     public function getInformation($uuid)
     {
-        $pokemon = $this->getPokemonByUuid($uuid);
+        $pokemon = $this->pokemonService->getPokemonByUuid($uuid);
 
         if ($pokemon === false) {
             return new JsonResponse(new \stdClass(), 404);
@@ -69,24 +69,10 @@ class PokemonController
         $type = $request->get('type');
         $level = (int)$request->get('level');
 
-        // TODO check type exists
-        if ($level <= 0) {
-            throw new InvalidArgumentException('Pokemon\'s level min level is 1');
-        }
-        if ($level > 30) {
-            throw new InvalidArgumentException('Pokemon\'s level could not exceed 30');
-        }
 
-        if (!in_array($type, ['pikachu', 'salameche', 'carapuce', 'bulbizare'])) {
-            throw new InvalidArgumentException('Unknown Pokemon\'s type !!! CALL PROF CHEN RIGHT NOOOW');
-        }
+        $pokemon = new Pokemon($uuid, $type, $level);
 
-        $sql = 'INSERT INTO pokemon.collection (uuid, type, level) VALUES (:uuid, :type, :level)';
-        $query = $this->connection->prepare($sql);
-        $query->bindValue('uuid', $uuid);
-        $query->bindValue('type', $type);
-        $query->bindValue('level', $level);
-        $query->execute();
+        $this->pokemonService->capturePokemon($pokemon);
 
         return new JsonResponse([
             'uuid' => $uuid,
@@ -102,58 +88,10 @@ class PokemonController
      */
     public function evolve($uuid)
     {
-        $pokemon = $this->getPokemonByUuid($uuid);
+        $pokemon = $this->pokemonService->getPokemonByUuid($uuid);
 
-        if ($pokemon['level'] < 7) {
-            throw new InvalidArgumentException('Oh maaaan ! This pokemon is too young to evolve :(');
-        }
-        if ($pokemon['level'] > 30) {
-            throw new InvalidArgumentException('Pokemon\'s level could not exceed level 30');
-        }
-
-        switch (true) {
-            case $pokemon['type'] === "salameche" && $pokemon['level'] >= 7:
-                $this->evolvePokemon($pokemon, 'reptincele');
-                break;
-            case $pokemon['type'] === "reptincele" && $pokemon['level'] >= 15;
-                $this->evolvePokemon($pokemon, 'dracofeu');
-                break;
-            case $pokemon['type'] === "carapuce" && $pokemon['level'] >= 7:
-                $this->evolvePokemon($pokemon, 'carabaffe');
-                break;
-            case $pokemon['type'] === "carabaffe" && $pokemon['level'] >= 15;
-                $this->evolvePokemon($pokemon, 'tortank');
-                break;
-        }
-        return $this->getInformation($uuid);
+        return new JsonResponse($pokemon);
     }
 
-    /**
-     * @param array $pokemon
-     * @param $newType
-     */
-    private function evolvePokemon(array $pokemon, $newType)
-    {
-        $sql = 'UPDATE pokemon.collection SET type = :type WHERE uuid = :uuid';
-        $query = $this->connection->prepare($sql);
-        $query->bindValue('uuid', $pokemon['uuid']);
-        $query->bindValue('type', $newType);
-        $query->execute();
-    }
 
-    /**
-     * @param $uuid
-     *
-     * @return mixed
-     */
-    private function getPokemonByUuid($uuid)
-    {
-        $sql = 'SELECT uuid, type, level FROM pokemon.collection WHERE uuid = :uuid';
-        $query = $this->connection->prepare($sql);
-        $query->bindValue('uuid', $uuid);
-        $query->execute();
-
-        $pokemon = $query->fetch();
-        return $pokemon;
-    }
 }
